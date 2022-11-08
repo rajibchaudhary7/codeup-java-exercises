c60e7f7d4e4d5f95aa97d57a11c700c08a23ad81
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-11-08 11:54:11 -0500

    java syntax, types, and variables exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,33 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("");
         System.out.print("Hello, World!");
+
+//Create an int variable named myFavoriteNumber and assign your
+// favorite number to it, then print it out to the console.
+        int myFavoriteNumber = 5;
+        System.out.println("\n");
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Billiards";
+        System.out.println(myString);
+
+        myString = "A";
+        System.out.println(myString);
+//        myString with a character value will be assigned the reverse cannot
+
+//        myString = 3.14159;
+//        my string will only accept a string value not number
+
+        long myNumber;
+        System.out.println(myNumber);
+//        Error appears: variable myNumber might not have been initialized
+
+//        long myNumber = 3.14;
+//        System.out.println(myNumber);
+
+
+
+
+
     }
 }
