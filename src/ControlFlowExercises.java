d91b43eea7784ed8d367fcce726e6f8f5671aa39
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-11-09 13:06:08 -0500

    Do while loop to square the loop that starts at 2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -4,15 +4,25 @@ public class ControlFlowExercises {
 //       while (i <= 15) {
 //           System.out.print(i + " "); i++;
 //       }
+
+
 //       int i = 0;
 //       do {
 //            System.out.println(i);
 //            i += 2;
 //       }while(i <= 100);
-        int i = 100;
-        do {
-            System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
+
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+        long i = 2;
+        do{
+            System.out.println(i * i);
+            i = i * i;
+        } while ( i <= 1000000);
     }
 }
