e89a3d413c0bc4dd795fd70500f752e8645f6fa4
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-11-09 13:15:56 -0500

    replacing while with for loop for 100 to -10 output

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -14,6 +14,9 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i += 2;
 //       }while(i <= 100);
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
 
 
 //        int i = 100;
@@ -21,6 +24,11 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i -= 5;
 //        } while (i >= -10);
+        for (int i = 100; i >= -10; i -= 5) {
+            System.out.println(i);
+        }
+
+
 
 //        long i = 2;
 //        do{
