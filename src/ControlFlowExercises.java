8bbd888410fb41e3df4c2f3e2a5221745424f4db
Rajib Chaudhary <rajibchaudhary2020@gmail.com>
2022-11-09 12:35:44 -0500

    do-while loop, loop basics exercises completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,13 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-       int i = 5;
-       while (i <= 15) {
-           System.out.print(i + " "); i++;
-       }
-
+//       int i = 5;
+//       while (i <= 15) {
+//           System.out.print(i + " "); i++;
+//       }
+       int i = 0;
+       do {
+            System.out.println(i);
+            i += 2;
+       }while(i <= 100);
     }
 }
